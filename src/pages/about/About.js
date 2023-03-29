7d1f2be7d49e40bf7145a8c4cf6b8c3bdd3b6264
siham bordjah <sihamsissa1903@gmail.com>
2023-03-29 11:36:25 +0200

    ajouter main

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
-// import aboutBanner from "../../assets/images/banner/aboutBanner.png";
 import AboutBanner from "../../components/banner/AboutBanner";
 import Collapse from "../../components/collapse/Collapse";
 
 const About = () => {
   return (
     <div className="about">
+      <Header />
       <main>
-        <Header />
         <AboutBanner />
         <Collapse />
-        <Footer />
       </main>
+
+      <Footer />
     </div>
   );
 };
