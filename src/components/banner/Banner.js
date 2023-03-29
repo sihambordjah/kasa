f28bbeeedab288ee12ef9b0239af8542a442113b
siham bordjah <sihamsissa1903@gmail.com>
2023-03-29 11:38:00 +0200

    metre a jours la banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,7 +1,7 @@
 export default function Banner() {
   return (
     <div className="banner-container">
-      <div className="banner-text-container">
+      <div className="banner-container__text">
         <div className="banner-text">Chez vous, partout et ailleurs</div>
       </div>
     </div>
