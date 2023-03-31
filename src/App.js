14162a470d72c9ddf862b7fb96da81445494b833
siham bordjah <sihamsissa1903@gmail.com>
2023-03-31 11:24:03 +0200

    lien

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import About from "./pages/about/About";
 import Home from "./pages/home/Home";
 import FichLogement from "./pages/logement/FichLogment";
@@ -7,15 +7,15 @@ import NoPage from "./pages/error/NoPage";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<Navigate replace to="/accueil" />} path="/" />
-        <Route path="/" element={<Home />} />
+        <Route path="/accueil" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/logement/:id" element={<FichLogement />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
