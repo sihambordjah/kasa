7c13feeff6abadff802d11449bca6ce9d8ec0c42
siham bordjah <sihamsissa1903@gmail.com>
2023-04-07 11:08:00 +0200

    app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles/app.scss";
-import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import About from "./pages/about/About";
 import Home from "./pages/home/Home";
 import FichLogement from "./pages/logement/FichLogment";
@@ -10,10 +10,10 @@ const App = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Navigate replace to="/accueil" />} />
-        <Route index element={<Home />} />
+        {/* <Route path="/" element={<Navigate replace to="/accueil" />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="logement/:id" element={<FichLogement />} />
+        <Route path="/logement/:id" element={<FichLogement />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
     </HashRouter>
