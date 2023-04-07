eeacbae8ec50fe5d55ee8072c4403b64c57c7c04
siham bordjah <sihamsissa1903@gmail.com>
2023-04-07 06:00:22 +0200

    mis a jours

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/index.scss";
-
+// ReactDOM pour s'attacher à notre HTML.
 const root = ReactDOM.createRoot(document.getElementById("root"));
+//dans la variable root on affiche tout mon application on importe
+// le composant parent App.js
 root.render(
   <React.StrictMode>
     <App />
