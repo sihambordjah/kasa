0d7be56c0be195eae5463ee863bcad14e2a350ba
siham bordjah <sihamsissa1903@gmail.com>
2023-04-07 11:13:26 +0200

    mis a jours

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles/app.scss";
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import About from "./pages/about/About";
 import Home from "./pages/home/Home";
 import FichLogement from "./pages/logement/FichLogment";
@@ -10,8 +10,8 @@ const App = () => {
   return (
     <HashRouter>
       <Routes>
-        {/* <Route path="/" element={<Navigate replace to="/accueil" />} /> */}
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate replace to="/accueil" />} />
+        <Route path="/accueil" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/logement/:id" element={<FichLogement />} />
         <Route path="*" element={<NoPage />} />
